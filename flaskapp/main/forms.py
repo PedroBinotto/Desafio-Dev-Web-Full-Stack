87c51cd0e5232e88e40143382b6970f695bd7a48
Pedro Binotto <pedrosantibinotto@gmail.com>
2021-03-15 17:30:29 -0300

    *Página de Cálculos finalizada

--- flaskapp/main/forms.py
+++ flaskapp/main/forms.py
@@ -3,7 +3,7 @@ from wtforms import IntegerField, SubmitField
 from wtforms.validators import InputRequired, ValidationError
 
 class InputForm(FlaskForm):
-	num1 = IntegerField(validators=[InputRequired('Você precisa informar um número!')], render_kw={'placeholder': 'Inserir Valor do Número...'})
-	num2 = IntegerField(validators=[InputRequired('Você precisa informar um número!')], render_kw={'placeholder': 'Inserir Valor do Número...'})
+	num1 = IntegerField(validators=[InputRequired('Você precisa informar um número!')], render_kw={'placeholder': 'Inserir Valor do Número (1)'})
+	num2 = IntegerField(validators=[InputRequired('Você precisa informar um número!')], render_kw={'placeholder': 'Inserir Valor do Número (2)'})
 
 	submit =  SubmitField('Calcular Números')
--- flaskapp/main/routes.py
+++ flaskapp/main/routes.py
@@ -9,12 +9,14 @@ main = Blueprint('main', __name__)
 @main.route('/', methods=['GET', 'POST'])
 @main.route('/home/<string:num1>/<string:num2>', methods=['GET', 'POST'])
 def home(num1=None, num2=None):
+	title = 'Calcular'
 	form = InputForm()
 	if form.validate_on_submit():
 		record = Record(num1=form.num1.data, num2=form.num2.data)
 		db.session.add(record)
 		db.session.commit()
 		return redirect(url_for('main.home',
+			title=title,
 			num1=str(form.num1.data) if form.num1.data >= 0 else '0',
 			num2=str(form.num2.data) if form.num2.data >= 0 else '0'
 		))
@@ -22,13 +24,19 @@ def home(num1=None, num2=None):
 		lim_inf = int(min([num1, num2]))
 		lim_sup = int(max([num1, num2]))
 		results = calculate(lim_inf, lim_sup)
-		return render_template('home.html', form=form, results=results)
-	return render_template('home.html', form=form)
+		return render_template('home.html',
+			title=title,
+			form=form,
+			results=results,
+			lim_inf=lim_inf,
+			lim_sup=lim_sup
+		)
+	return render_template('home.html', title=title,form=form)
 
 @main.route('/history')
 def history():
 	page = request.args.get('page', 1, type=int)
-	records = Record.query.order_by(Record.date_added.desc()).paginate(page=page, per_page=5)
+	records = Record.query.order_by(Record.date_added.desc()).paginate(page=page, per_page=50)
 	next_url = url_for('main.history', page=records.next_num)\
 		if records.has_next else None
 	prev_url = url_for('main.history', page=records.prev_num)\
